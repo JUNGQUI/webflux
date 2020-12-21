b0ea84e08e28df70a87b9f0b909e1c493ea0906d
JUNGQUI <ljk2518@gmail.com>
2020-12-21 11:24:29 +0900

    수정사항 및 버그 발견
    - observer pattern 완성하지 않음;;
    
    TODO
    - test case 에 assertion 으로 통과 시키기

--- src/test/java/com/practice/jk/webflux/ob/ObTest.java
+++ src/test/java/com/practice/jk/webflux/ob/ObTest.java
@@ -1,8 +1,11 @@
 package com.practice.jk.webflux.ob;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,5 +42,26 @@ class ObTest {
 		for (int i = 0; i < 10; i++) {
 			Assertions.assertEquals(integerList.get(i), integerIterable.next());
 		}
+	}
+
+	@Test
+	void observerTest() {
+		List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		List<Integer> integerByObserver = new ArrayList<>();
+
+		Observer observer = new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				integerByObserver.add((Integer) arg);
+				System.out.println(arg);
+			}
+		};
+
+		Ob intObserver = new Ob();
+		intObserver.addObserver(observer);
+
+//		for (int i = 0; i < 10; i++) {
+//			Assertions.assertEquals(integerList.get(i), integerByObserver.get(i));
+//		}
 	}
 }
