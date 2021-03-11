e5acdd4a1ac2aafec3058d3d579363f59ab62dc0
JUNGQUI <ljk2518@gmail.com>
2021-03-11 17:03:46 +0900

    결론 : parallel
    - 병렬 처리가 하나의 요청에 대해 여러 thread 가 아닌, 여러 요청에 대해 병렬 처리
    - 내가 생각했던건 1개의 요청으로도 service 내에서 여러 갈래로 나눌 수 있다고 생각함

--- src/test/java/com/practice/jk/webflux/monoflux/MonoFluxTest.java
+++ src/test/java/com/practice/jk/webflux/monoflux/MonoFluxTest.java
@@ -1,10 +1,17 @@
 package com.practice.jk.webflux.monoflux;
 
+import com.practice.jk.webflux.monoflux.service.MonoFluxService;
+import java.util.function.Predicate;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class MonoFluxTest {
+
+	@Autowired
+	private MonoFluxService service;
 
 	@Test
 	void monoFluxTest() {
@@ -14,5 +21,16 @@ class MonoFluxTest {
 	@Test
 	void publishByFlux() {
 		MonoFlux.publishByFlux();
+	}
+
+	@Test
+	void multiThread() {
+		try {
+			StepVerifier.create(service.multiMono())
+					.expectNextMatches(Predicate.isEqual("long\nsmall"))
+					.verifyComplete();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
